86b89659ce5643c60318c3d5da495ff908eb3e78
Brent Jackson <jxnblk@gmail.com>
2013-03-28 00:01:29 -0700

    No idea what I'm doing here.

--- Twipster For Chrome/events.js
+++ Twipster For Chrome/events.js
@@ -1,10 +1,6 @@
-
-  	chrome.extension.onMessage.addListener(function(request, sender, sendMessage) {
-        if (request.method == "getFontSize")
-          sendMessage({status: localStorage['twipsterFontSize']});
-        else
-          sendMessage({}); // snub them.
-    });
-
-
-
+chrome.extension.onMessage.addListener(function(request, sender, sendMessage) {
+    if (request.method == "getFontSize")
+      sendMessage({status: localStorage['twipsterFontSize']});
+    else
+      sendMessage({}); // snub them.
+});
--- Twipster For Chrome/twipster.js
+++ Twipster For Chrome/twipster.js
@@ -1,10 +1,15 @@
+$(document).ready(function(){
+
   chrome.extension.sendMessage({method: "getFontSize"}, function(response) {
-    console.log(response.status);
     if (response.status){
       var twipsterFontSize = response.status;
     } else {
-      var twipsterFontSize = 16;
+      var twipsterFontSize = 20;
     };
-    console.log(twipsterFontSize);
+    //$("body").addClass("tfs-" + twipsterFontSize); // Seems really hacky, but seemed to load faster.
+  
+    // This may be a better method for setting font-size, but has timing issues.
     $("<style id='twipster-settings' type='text/css'>.js-tweet-text { font-size: " + twipsterFontSize + "px !important; }</style>").appendTo("head");
   });
+
+});
